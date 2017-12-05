0ccb6962c051072e56047854b39661428d00bcf5
David Cho <hoontime@Davidui-MacBook-Pro.local>
2017-12-06 00:49:01 +0100

    parameters to file

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.set('port', (process.env.PORT || 5000))
 const REQUIRE_AUTH = true
 const AUTH_TOKEN = 'an-example-token'
 
+//make files
+var fs = require('fs');
+
+//blocking the stream
+//var readMe = fs.readFileSync('readMe.txt', 'utf8');
+//fs.readFile('readMe.txt', 'utf8', function(err,data){
+//    fs.writeFile('writeMe.txt', data);
+//});
+//console.log(readMe);
+//fs.writeFileSync('writeMe.txt', readMe);
+
 app.get('/', function (req, res) {
   res.send('Use the /webhook endpoint.')
 })
@@ -18,7 +29,7 @@ app.post('/webhook', function (req, res) {
   // we expect to receive JSON data from api.ai here.
   // the payload is stored on req.body
   console.log(req.body)
-
+  
   // we have a simple authentication
   if (REQUIRE_AUTH) {
     if (req.headers['auth-token'] !== AUTH_TOKEN) {
@@ -34,9 +45,28 @@ app.post('/webhook', function (req, res) {
   // the value of Action from api.ai is stored in req.body.result.action
   console.log('* Received action -- %s', req.body.result.action)
 
-  // parameters are stored in req.body.result.parameters
-  var userName = req.body.result.parameters['given-name']
-  var webhookReply = ':wave: ' + userName + '! I am CoffeeMeBot.'
+  var intentName = req.body.result.metadata.intentName;
+  var webhookReply = "";
+    
+  if ( intentName === "Order Welcome"){
+    // parameters are stored in req.body.result.parameters
+    var userName = req.body.result.parameters['given-name'];
+    webhookReply = ':wave: ' + userName + ' I am CoffeeMeBot.:coffee: :smiley: :robot_face:\n'+
+                   'Please order your Coffee';    
+  }else if ( intentName === "Coffee Order"){
+    var coffee = req.body.result.parameters['Coffee'];
+    var size = req.body.result.parameters['Size'];
+    var dairy = req.body.result.parameters['Dairy'];
+    var hotOrIced = req.body.result.parameters['Hot-or-Ice'];
+      
+    if( coffee && size && hotOrIced){
+        var datetime = new Date();
+        var data = coffee + ',' + size + ',' + hotOrIced + ',' + dairy + ',' + datetime;
+        fs.writeFileSync('orderFile.txt', data);
+        console.log(fs.readFileSync('orderFile.txt', 'utf8'));
+    }
+  }
+  
 
   // the most basic response
   res.status(200).json({
