0058e83f49b35930dbe98e08d1ac93e461e72431
David Cho <hoontime@Davidui-MacBook-Pro.local>
2017-12-07 14:38:08 +0100

    order time setting

--- utilFunction.js
+++ utilFunction.js
@@ -44,82 +44,82 @@ module.exports = {
         var price = 0.00;
         
         switch (coffee){
-            case "Caffe Americano"
+            case "Caffe Americano":
                 switch (size){
-                    case "Short"
+                    case "Short":
                         price = 0.80;
                         break;
-                    case "Tall"
+                    case "Tall":
                         price = 1.00;
                         break;
-                    case "Grande"
+                    case "Grande":
                         price = 1.20;
                         break;
-                    case "Venti"
+                    case "Venti":
                         price = 1.40;
                         break;
                 }
                 break;
-            case "Caffe Latte"
+            case "Caffe Latte":
                 switch (size){
-                    case "Short"
+                    case "Short":
                         price = 1.00;
                         break;
-                    case "Tall"
+                    case "Tall":
                         price = 1.20;
                         break;
-                    case "Grande"
+                    case "Grande":
                         price = 1.40;
                         break;
-                    case "Venti"
+                    case "Venti":
                         price = 1.60;
                         break;
                 }
                 break;
-            case "Cappuccino"
+            case "Cappuccino":
                 switch (size){
-                    case "Short"
+                    case "Short":
                         price = 1.00;
                         break;
-                    case "Tall"
+                    case "Tall":
                         price = 1.20;
                         break;
-                    case "Grande"
+                    case "Grande":
                         price = 1.40;
                         break;
-                    case "Venti"
+                    case "Venti":
                         price = 1.60;
                         break;
                 }
                 break;
-            case "Caffe Mocha"
+            case "Caffe Mocha":
                 switch (size){
-                    case "Short"
+                    case "Short":
                         price = 1.20;
                         break;
-                    case "Tall"
+                    case "Tall":
                         price = 1.40;
                         break;
-                    case "Grande"
+                    case "Grande":
                         price = 1.60;
                         break;
-                    case "Venti"
+                    case "Venti":
                         price = 1.80;
                         break;
                 }
                 break;
-            case "Brewed Coffee"
+            case "Brewed Coffee":
                 switch (size){
-                    case "Short"
+                    case "Short":
                         price = 0.70;
                         break;
-                    case "Tall"
+                    case "Tall":
                         price = 0.90;
                         break;
-                    case "Grande"
+                    case "Grande":
                         price = 1.10;
                         break;
-                    case "Venti"
+                    case "Venti":
                         price = 1.30;
                         break;
                 }
