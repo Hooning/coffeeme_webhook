f6a5667f07073ac2b10fc14b003a2b98752df243
David Cho <hoontime@Davidui-MacBook-Pro.local>
2017-12-07 13:00:22 +0100

    menu function add

--- index.js
+++ index.js
@@ -39,115 +39,128 @@ app.post('/webhook', function (req, res) {
     var userName = "David";
     var fileNm = userName + '.txt';
     var userType = userManage.checkUser(fileNm);
-    
+
     var greetings = "";
 
     if (actionName === "input.welcome" && userName !== "") {
         console.log("## input.welcome Action in with user-name ##")
         // parameters are stored in req.body.result.parameters
-        
-        if( userType === "newUser"){
+
+        if (userType === "newUser") {
             greetings += "Nice to meet you ";
             greetings += userName.toString();
-            greetings += ":wave: \nPlease order your Coffee :coffee:";    
-        }else{
+            greetings += ":wave: \nPlease order your Coffee :coffee:";
+        } else {
             greetings += "Welcome back ";
             greetings += userName.toString();
             greetings += ":sunglasses:\nWhich coffee is in your mind?"
         }
-        
+
         greetings = greetings.toString();
-          
-        var webhookReply = 
-            {
-                "slack": {
-                    "text": greetings,
-                    "attachments": [
-                        {
-                            "text": "You can simply type or choose from here..",
-                            "fallback": "You are unable to choose a coffee",
-                            "callback_id": "wopr_coffee",
-                            "color": "#724f0c",
-                            "attachment_type": "default",
-                            "actions": [
-                                {
-                                    "name": "Coffee",
-                                    "text": "Caffe Americano",
-                                    "type": "button",
-                                    "value": "Caffe Americano"
+
+        var webhookReply = {
+            "slack": {
+                "text": greetings,
+                "attachments": [
+                    {
+                        "text": "You can simply type or choose from here..",
+                        "fallback": "You are unable to choose a coffee",
+                        "callback_id": "wopr_coffee",
+                        "color": "#724f0c",
+                        "attachment_type": "default",
+                        "actions": [
+                            {
+                                "name": "Coffee",
+                                "text": "Caffe Americano",
+                                "type": "button",
+                                "value": "Caffe Americano"
                                 },
-                                {
-                                    "name": "Coffee",
-                                    "text": "Caffe Latte",
-                                    "type": "button",
-                                    "value": "Caffe Latte"
+                            {
+                                "name": "Coffee",
+                                "text": "Caffe Latte",
+                                "type": "button",
+                                "value": "Caffe Latte"
                                 },
-                                {
-                                    "name": "Coffee",
-                                    "text": "Cappuccino",
-                                    "type": "button",
-                                    "value": "Cappuccino"
+                            {
+                                "name": "Coffee",
+                                "text": "Cappuccino",
+                                "type": "button",
+                                "value": "Cappuccino"
                                 },
-                                {
-                                    "name": "Coffee",
-                                    "text": "Caffe Mocha",
-                                    "type": "button",
-                                    "value": "Caffe Mocha"
+                            {
+                                "name": "Coffee",
+                                "text": "Caffe Mocha",
+                                "type": "button",
+                                "value": "Caffe Mocha"
                                 },
-                                {
-                                    "name": "Coffee",
-                                    "text": "Brewed Coffee",
-                                    "type": "button",
-                                    "value": "Brewed Coffee"
+                            {
+                                "name": "Coffee",
+                                "text": "Brewed Coffee",
+                                "type": "button",
+                                "value": "Brewed Coffee"
                                 },
-                                {
-                                    "name": "Coffee",
-                                    "text": "Espresso",
-                                    "type": "button",
-                                    "value": "Espresso"
+                            {
+                                "name": "Coffee",
+                                "text": "Espresso",
+                                "type": "button",
+                                "value": "Espresso"
                                 }
-                            ]   
+                            ]
                         }
                     ]
-                }
             }
+        }
+
+    } else if (actionName === "input.orderfrommenu") {
+        var coffee = req.body.result.contexts[0].parameters['Coffee'];
+        var size = req.body.result.contexts[0].parameters['Size'];
+        var price = req.body.result.contexts[0].parameters['price'];
         
-    } else if (actionName === "input.orderdone") {
+        console.log("## coffee : " + coffee);
+        console.log("## size : " + size);
+        console.log("## price : " + price);
+        
+        res.body.result.contexts[1].parameters['Coffee'] = coffee;
+        res.body.result.contexts[1].parameters['Size'] = size;
+        res.body.result.contexts[1].parameters['price'] = price;
+        
+    }else if (actionName === "input.orderdone") {
         var coffee = req.body.result.contexts[0].parameters['Coffee'];
         var size = req.body.result.contexts[0].parameters['Size'];
         var dairy = req.body.result.contexts[0].parameters['Dairy'];
         var hotOrIced = req.body.result.contexts[0].parameters['Hot-or-Ice'];
-        var inputTime = req.body.result.contexts[0].parameters['time']; 
+        var inputTime = req.body.result.contexts[0].parameters['time'];
         var today = new Date();
         var deliveryTime = "";
-        
-        if( inputTime === ""){
-            deliveryTime = utilFunc.getTime(today);    
-        }else{
+
+        if (inputTime === "") {
+            deliveryTime = utilFunc.getTime(today);
+        } else {
             console.log("## InputTime : " + inputTime);
             deliveryTime = inputTime;
         }
-        
+
         console.log("## coffee : " + coffee);
         console.log("## size : " + size);
         console.log("## hotOrIced : " + hotOrIced);
         console.log("## deliveryTime : " + deliveryTime);
-        
+
         if (coffee && size && hotOrIced && deliveryTime) {
             console.log("## Mandatory field success!");
             //Coffee|Size|HotorIced|Dairy|DeliverTime|OrderDateTime|
             var data = coffee + ',' + size + ',' + hotOrIced + ',' + dairy + ',' + deliveryTime + ',' + today;
-            
+
             //Create File
             fs.writeFileSync(fileNm, data);
-            console.log("## FILE ["+fileNm+"] has been made."); console.log("## Read File ##\n"+fs.readFileSync(fileNm, 'utf8'));
+            console.log("## FILE [" + fileNm + "] has been made.");
+            console.log("## Read File ##\n" + fs.readFileSync(fileNm, 'utf8'));
         }
-    } else{
+    } else {
         console.log("## Action not catched!! ##");
     }
-    
+
     //console.log("## webhookReply : %j", webhookReply);
-    
+
     // the most basic response
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json({
